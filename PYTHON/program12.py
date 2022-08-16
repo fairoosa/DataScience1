fa0265fdb8689e36106d0af3faa72f8aafcc3e08
fairoosa <67853077+fairoosa@users.noreply.github.com>
2022-08-16 12:07:03 +0530

    Add files via upload

---
+++ PYTHON/program12.py
@@ -0,0 +1,13 @@
+import numpy
+matrix1=numpy.matrix([[2,2,2],[2,2,2],[2,2,2]])
+matrix2=numpy.matrix([[2,2,2],[2,2,2],[2,2,2]])
+matrix3=numpy.add(matrix1,matrix2)
+matrix4=numpy.subtract(matrix1,matrix2)
+matrix5=numpy.matmul(matrix1,matrix2)
+matrix6=2*matrix1
+matrix7=numpy.transpose(matrix1)
+print(matrix3)
+print(matrix4)
+print(matrix5)
+print(matrix6)
+print(matrix7)
