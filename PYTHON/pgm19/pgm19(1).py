2c7bffab02cfbceec8f12c0f61d1d97e051b4c0c
fairoosa <67853077+fairoosa@users.noreply.github.com>
2022-08-22 11:54:06 +0530

    Update pgm19(1).py

--- PYTHON/pgm19/pgm19(1).py
+++ PYTHON/pgm19/pgm19(1).py
@@ -6,8 +6,8 @@ colors = {"Setosa":"red", "Versicolor":"green",
 "Virginica":"blue"}
 grouped = df.groupby("species")
 for key, group in grouped:
-    group.plot(ax=ax, kind="scatter", x="petal.length",
-y="petal.width", label=key, color=colors[key])
+    group.plot(ax=ax, kind="scatter", x="sepal.length",
+y="sepal.width", label=key, color=colors[key])
 ax.set_title("Scatter Plot")
 ax.set_xlabel("sepal Length")
 ax.set_ylabel("sepal Width")
